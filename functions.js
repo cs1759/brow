8055efe668b4934e65e5dedd862a9149dfa111b6
cs1759 <63865245+cs1759@users.noreply.github.com>
2021-12-11 00:08:43 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,72 @@
+const os = require('os')
+const util = require('util')
+const fs = require('fs')
+const childProcess = require('child_process')
+const exec = util.promisify(childProcess.exec)
+
+const startBrowser = async (browser, url) => {
+    try {
+        if (os.type() == 'Windows_NT')
+            await exec(`start ${browser} ${url}`)
+        else if (os.type() == 'Linux')
+            await exec(`${browser == 'chrome' ? 'google-chrome' : 'firefox'} ${url}`)
+        else
+            await exec(`open -a ${browser == 'chrome' ? '"Google Chrome"' : 'Firefox '} ${url}`)
+
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return browser + " Browser running " + url;
+
+}
+
+const stopBrowser = async (browser) => {
+    try {
+        if (os.type() == 'Windows_NT')
+            await exec(`taskkill /F /IM ${browser}.exe`)
+        else if (os.type() == 'Linux')
+            await exec(`pkill -9 ${browser}`)
+        else
+            await exec(`killall ${browser}`)
+
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return browser + " Browser stoped ";
+}
+
+
+const getURL = async (browser) => {
+    var path = '';
+    if (browser == 'chrome') {
+        try {
+            await stopBrowser('chrome')
+        } catch (err) {
+            return err;
+        } finally {
+            var string = await exec(String.raw`.\chrome-session-dump.exe -active "C:\Users\91790\AppData\Local\Google\Chrome\User Data\Default\Extensions\efaidnbmnnnibpcajpcglclefindmkaj\15.1.3.2_0\lib\session.js" `)
+            return string;
+        }
+    }
+
+    else {
+        var result = await exec('python recovery.py')
+        return result;
+    }
+}
+
+const cleanUp = (browser) => {
+
+}
+
+
+
+module.exports = {
+    startBrowser,
+    stopBrowser,
+    getURL
+}
